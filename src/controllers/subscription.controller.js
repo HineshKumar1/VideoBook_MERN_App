5619be8b486162d10aa1f4282d21a5731d965af4
HineshKumar1 <hineshkumar46@gmail.com>
2024-04-07 15:47:56 +0500

    Fix import and add getUserChannelSubscribers route

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,4 +1,5 @@
-import { subscription} from "../models/subscription.model.js";
+import mongoose from "mongoose";
+import { subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -6,10 +7,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleSubscription = asyncHandler(async (req, res) => {
   try {
     const { channelId } = req.params;
-    
+
     const subscribe = await subscription.create({
       channel: channelId,
-        subscriber: req.user._id,
+      subscriber: req.user._id,
     });
     return res
       .status(200)
@@ -22,6 +23,57 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   }
 });
 
-export { 
-    toggleSubscription 
-};
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  try {
+    const { channelId } = req.params;
+
+    const userSubscribedChannels = await subscription.aggregate([
+      {
+        $match: {
+          channel: new mongoose.Types.ObjectId(channelId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscriber",
+          foreignField: "_id",
+          as: "subscribers",
+        },
+      },
+      {
+        $project: {
+          subscribers: {
+            _id:1,
+           userName:1,
+            fullName:1,
+            avatar:1
+          },
+        },
+      },
+      // {
+      //   $addFields: {
+      //     channelDetails: {
+      //       $arrayElemAt: ["$channels", 0], // Corrected usage of $arrayElemAt
+      //     },
+      //   },
+      // },
+    ]);
+    console.log(userSubscribedChannels);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          userSubscribedChannels,
+          "All user channel Subscribes fetched Successfull!!"
+        )
+      );
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(500, "Internal Server Error");
+  }
+});
+
+export { toggleSubscription, getUserChannelSubscribers };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { toggleSubscription} from "../controllers/subscription.controller.js";
+import { getUserChannelSubscribers, toggleSubscription} from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/subscribe/:channelId").post(verifyJWT, toggleSubscription);
+router.route("/subscribe/channel/:channelId").get(verifyJWT, getUserChannelSubscribers);
 
 export default router;
